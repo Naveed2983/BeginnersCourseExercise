b3b0a705084db7388868220680efafba50d26346
MNaveedAhmad298 <mnaveed.abid786@gmail.com>
2025-07-09 16:23:57 +0500

    beginner course exercises.

---
+++ BeginnersCourseExercise/CondationalExercise.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnersCourse
+{
+    public class Exercise1
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter your Number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 0 && number <= 10)
+            {
+                Console.WriteLine("Number is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Valid.");
+            }
+            Console.ReadKey();
+            //Can aslo write like that.
+            //Console.WriteLine(number > 0 && number <= 10 ? "Number is Valid " : "Number is not Valid");
+        }
+    }
+    public class Exercise2
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter Number 1: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number 2: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 == number2)
+                Console.WriteLine("Both are Same.");
+            else if (number1 > number2)
+                Console.WriteLine("Number {0} is Greater", number1);
+            else Console.WriteLine("Number {0} is Greater", number2);
+
+            Console.ReadKey();
+        }
+    }
+    public class Exercise3
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter the Width of the Image : ");
+            float width = float.Parse(Console.ReadLine());
+            Console.Write("Enter the Height of the Image : ");
+            float height = float.Parse(Console.ReadLine());
+
+            if (width == height)
+            {
+                Console.WriteLine("It is a Square Image. ");
+            }
+            else if (width > height)
+            {
+                Console.WriteLine("It is a LandScape Image. ");
+            }
+            else
+            {
+                Console.WriteLine("It is a Potrait Image. ");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Exercise4
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter the Speed Limit: ");
+            int speedLimit = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Speed of Car: ");
+            int speedOfCar = int.Parse(Console.ReadLine());
+
+            if (speedOfCar < speedLimit)
+            {
+                Console.WriteLine("Speed is Okay!");
+            }
+            else
+            {
+                int points = CalculatePoints(speedOfCar, speedLimit);
+                if (points > 12)
+                    Console.WriteLine("License Suspended as your Points are {0} ", points);
+                else
+                    Console.WriteLine("Warning Issued!! Your Points are : {0} ", points);
+            }
+            Console.ReadKey();
+        }
+        public static int CalculatePoints(int speedOfCar, int speedLimit)
+        {
+            int points = (speedOfCar - speedLimit) / 5;
+            return points;
+        }
+    }
+}
---
+++ BeginnersCourseExercise/Strings.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnersCourse
+{
+    public class Section8Ex1
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            numbers.Sort();
+            var isConsecutive = true;
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1)
+                {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+            if (isConsecutive)
+            {
+                Console.WriteLine("Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Not Consecutive");
+            }
+        }
+    }
+    public class Section8Ex2
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+                return;
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            var uniques = new List<int>();
+            var includesDuplicates = false;
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+                else
+                {
+                    includesDuplicates = true;
+                    break;
+                }
+            }
+            if (includesDuplicates)
+                Console.WriteLine("Duplicate");
+        }
+    }
+    public class Section8Ex3
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter time: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            var components = input.Split(':');
+            if (components.Length != 2)
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            var hour = Convert.ToInt32(components[0]);
+            var minute = Convert.ToInt32(components[1]);
+
+            if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid Time");
+        }
+    }
+    public class Section8Ex4
+    {
+        public static void exercise()
+        {
+            Console.WriteLine("Enter Some Words: ");
+            var str = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("Invalid Words. ");
+                return;
+            }
+
+            var PascalString = "";
+            var str1 = str.Split(' ');
+            foreach (var word in str1)
+            {
+                var Pascalword = char.ToUpper(word[0]) + word.Substring(1).ToLower();
+                PascalString += Pascalword;
+            }
+            Console.WriteLine("Pascal Form is : "+PascalString);
+
+
+        }
+    }
+    public class Section8Ex5
+    {
+        public static void exercise()
+        {
+            Console.Write("Enter a word: ");
+            var input = Console.ReadLine();
+            input = input.ToLower();
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'o', 'u', 'i' });
+            var vowelsCount = 0;
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+            Console.WriteLine("The Vowel Count is: "+vowelsCount);
+            Console.ReadKey();
+        }
+
+    }
+
+}
+
+
+
